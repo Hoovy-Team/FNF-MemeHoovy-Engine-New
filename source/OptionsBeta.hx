da42ae2d0873f13204886284184ac1297d20f21e
[504]brandon <97409501+504brandon@users.noreply.github.com>
2022-10-07 17:33:57 -0500

    added health color option

--- source/OptionsBeta.hx
+++ source/OptionsBeta.hx
@@ -8,7 +8,9 @@ import flixel.text.FlxText;
 class OptionsBeta extends MusicBeatState
 {
     var option_gt:FlxText;
+    var option_hpc:FlxText;
     public static var option_gt_int:Int = 0;
+    public static var option_hpc_int:Int = 0;
     override function create()//this takes alot of code from my cookie clicker game huh
     {
         super.create();
@@ -29,11 +31,21 @@ class OptionsBeta extends MusicBeatState
         option_gt.screenCenter(X);
 		add(option_gt);
 
+        option_hpc = new FlxText(0, 200, 0, "n/a", 30);
+		option_hpc.pixelPerfectPosition = true;
+		option_hpc.borderColor = FlxColor.BLACK;
+		option_hpc.borderSize = 3;
+		option_hpc.borderStyle = OUTLINE;
+        option_hpc.screenCenter(X);
+		add(option_hpc);
+
         FlxG.mouse.visible = true;
         FlxG.mouse.enabled = true;
 
         if (FlxG.save.data.option_gt_int != option_gt_int)
 			option_gt_int = FlxG.save.data.option_gt_int;
+        if (FlxG.save.data.option_hpc_int != option_hpc_int)
+			option_hpc_int = FlxG.save.data.option_hpc_int;
         super.create();
     }
 
@@ -47,6 +59,14 @@ class OptionsBeta extends MusicBeatState
         {
             option_gt.text = "Ghost Tapping: false";
         }
+        if(option_hpc_int == 1)
+        {
+            option_hpc.text = "Health Colors: true";
+        }
+        else if(option_hpc_int == 0)
+        {
+            option_hpc.text = "Health Colors: false";
+        }
         if(FlxG.mouse.overlaps(option_gt) && FlxG.keys.justPressed.ENTER)
         {
             option_gt_int++;
@@ -57,10 +77,24 @@ class OptionsBeta extends MusicBeatState
             option_gt_int--;
             FlxG.save.data.option_gt_int = option_gt_int;
         }
+        if(FlxG.mouse.overlaps(option_hpc) && FlxG.keys.justPressed.ENTER)
+        {
+            option_hpc_int++;
+            FlxG.save.data.option_hpc_int = option_hpc_int;
+        }
+        if (FlxG.mouse.overlaps(option_hpc) && FlxG.keys.justPressed.BACKSPACE)
+        {
+            option_hpc_int--;
+            FlxG.save.data.option_hpc_int = option_hpc_int;
+        }
         if(option_gt_int > 1)
             option_gt_int = 1;
         if(option_gt_int < 0)
             option_gt_int = 0;
+        if(option_hpc_int > 1)
+            option_hpc_int = 1;
+        if(option_hpc_int < 0)
+            option_hpc_int = 0;
         if(FlxG.keys.justPressed.ESCAPE)
         {
             FlxG.switchState(new MainMenuState());
