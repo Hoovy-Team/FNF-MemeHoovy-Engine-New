85f0c653e9452e30957d047647ed622d25e6529e
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-07 19:11:29 -0400

    think this is why it didn't work

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -49,12 +49,13 @@ class HealthIcon extends FlxSprite
 		{
 			if (!Assets.exists(Paths.image('icons/icon-' + char))){
 				loadGraphic(Paths.image('icons/icon-face'), true, 150, 150); // wouldn't it be ironic if it didn't exist?
+				animation.add('face', [0, 1], 0, false, isPlayer);
 			}
 			else {
 				loadGraphic(Paths.image('icons/icon-' + char), true, 150, 150);
 				animation.add(char, [0, 1], 0, false, isPlayer);
 			}
-			animation.add(char, [0, 1], 0, false, isPlayer);
+			// animation.add(char, [0, 1], 0, false, isPlayer);
 		}
 		animation.play(char);
 		this.char = char;
