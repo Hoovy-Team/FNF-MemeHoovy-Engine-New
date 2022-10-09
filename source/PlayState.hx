da61e1b59840609dfce67e0f6d21766a6d28513f
[504]brandon <97409501+504brandon@users.noreply.github.com>
2022-10-08 19:43:14 -0500

    i hate you git (anyway lots of code fixed

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -550,12 +550,11 @@ class PlayState extends MusicBeatState
 				tower.antialiasing = true;
 				add(tower);
 
-				var ground:FlxSprite = new FlxSprite(-420, -150).loadGraphic(Paths.image('tankGround'));
+				var ground:FlxSprite = new FlxSprite(-420, -150).loadGraphic(Paths.image('tank/tankGround'));
 				ground.scrollFactor.set();
 				ground.antialiasing = true;
 				ground.setGraphicSize(Std.int(ground.width * 1.15));
 				ground.scrollFactor.set(1, 1);
-
 				ground.updateHitbox();
 				add(ground);
 
@@ -641,7 +640,13 @@ class PlayState extends MusicBeatState
 				gfVersion = 'gf-pixel';
 			case 'schoolEvil':
 				gfVersion = 'gf-pixel';
+			case 'tank':
+				gfVersion = 'gf-tank';
 		}
+			if(gfVersion == 'gf-tank'){
+			gf.x -= 84.15;
+			gf.y += 10.2;
+			}
 
 		if (curStage == 'limo')
 			gfVersion = 'gf-car';
@@ -689,6 +694,8 @@ class PlayState extends MusicBeatState
 				dad.x -= 150;
 				dad.y += 100;
 				camPos.set(dad.getGraphicMidpoint().x + 300, dad.getGraphicMidpoint().y);
+			case 'tankman':
+				dad.y + 180;
 		}
 
 		boyfriend = new Boyfriend(770, 450, SONG.player1);
@@ -968,6 +975,7 @@ class PlayState extends MusicBeatState
 		generateStaticArrows(1);
 
 		talking = false;
+		Conductor.songPosition = 0;
 		startedCountdown = true;
 		Conductor.songPosition -= Conductor.crochet * 5;
 
@@ -1420,8 +1428,21 @@ class PlayState extends MusicBeatState
 		}
 
 		super.update(elapsed);
-
-		scoreTxt.text = 'Score:' + songScore + ' | Misses:' + songMisses + ' | Combo:' + combo + ' | Accuracy:' + FlxMath.roundDecimal(songAccuracy, 4) + ' | Time:' + FlxStringUtil.formatTime((FlxG.sound.music.length - FlxMath.bound(Conductor.songPosition, 0)) / 1000, false);
+				
+		switch(curStep){
+			case 60, 444, 524, 828:
+				if(SONG.song.toLowerCase() == 'ugh'){
+				dad.playAnim('Ugh', true);
+				FlxTween.tween(camGame, {zoom: 1.3}, 0.5, {ease: FlxEase.quadInOut});
+				}
+			case 736:
+				if(SONG.song.toLowerCase() == 'stress'){
+				dad.playAnim('PrettyGood', true);
+				FlxTween.tween(camGame, {zoom: 1.8}, 2, {ease: FlxEase.quadInOut});
+			}
+		}
+				
+		scoreTxt.text = 'Score:' + songScore + ' | Misses:' + songMisses + ' | Combo:' + combo + ' | Accuracy:' + FlxMath.roundDecimal(songAccuracy, 4) + '% | Time:' + FlxStringUtil.formatTime((FlxG.sound.music.length - FlxMath.bound(Conductor.songPosition, 0)) / 1000, false);
 		if(songAccuracy > 100)
 			songAccuracy = 100;
 
