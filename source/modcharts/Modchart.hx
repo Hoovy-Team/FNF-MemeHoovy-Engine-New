1238a09723bdfb44b74eb7737c3993353259e249
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-12-05 00:29:34 -0500

    untested code becuz I don't wanna have unpushed changes forever

--- source/modcharts/Modchart.hx
+++ source/modcharts/Modchart.hx
@@ -3,8 +3,10 @@ package modcharts;
 import flixel.tweens.FlxTween;
 import flixel.FlxSprite;
 import flixel.tweens.FlxEase;
+import flixel.FlxG;
 
 // VERY UNFINISHED CODE
+// modchart support because yes (and also psych engine has modchart support soooo)
 
 class Modchart {
     public function new() {}
@@ -14,7 +16,7 @@ class Modchart {
      * @param strum The strum to rotate. Might have to use `strumLineNotes.forEachAlive`
      * @param amount Amount to rotate. Example : 500
      */
-    public static inline function rotateStrum(strum:FlxSprite, amount:Float) {
+    public static inline function rotateStrum(strum:FlxSprite, amount:Float):Void {
         strum.angle += Math.PI * amount;
     }
 
@@ -24,5 +26,13 @@ class Modchart {
      */
     public static inline function coolRotations(strum:FlxSprite):Void {
         FlxTween.tween(strum, {angle: 360}, Conductor.crochet / 1000 * 2, {ease: FlxEase.cubeInOut});
+    }
+
+    /**
+     * [Description] Moves the notes in a snake-ish pattern.
+     * @param note The note(s) to move.
+     */
+    public static inline function wigglyNotes(note:Note):Void {
+        note.x = note.x + (Math.sin(FlxG.elapsed * 2) + Conductor.normalizedCrochet / 1000);
     }
 }
